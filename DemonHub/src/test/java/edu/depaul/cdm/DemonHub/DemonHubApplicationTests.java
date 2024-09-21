79772feaea8accadbe22e05b7874f63ae07cf6a2
serhat111 <49848798+serhat111@users.noreply.github.com>
2024-09-20 22:35:34 -0500

    Add files via upload

---
+++ DemonHub/src/test/java/edu/depaul/cdm/DemonHub/DemonHubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.DemonHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
